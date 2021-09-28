21ef95cf679e8ab2d980b5cdb91ee83638df2055
Migz93 <33037112+Migz93@users.noreply.github.com>
2021-09-28 12:30:20 +0100

    Add retain option

--- rtl2mqtt.py
+++ rtl2mqtt.py
@@ -87,7 +87,7 @@ while True:
             sys.exit(rtl433_proc.poll())
 
         if "time" in line:
-            mqttc.publish(MQTT_TOPIC, payload=line, qos=MQTT_QOS, retain=True)
+            mqttc.publish(MQTT_TOPIC, payload=line, qos=MQTT_QOS, retain=MQTT_RETAIN)
             json_dict = json.loads(line)
             for item in json_dict:
                 value = json_dict[item]
